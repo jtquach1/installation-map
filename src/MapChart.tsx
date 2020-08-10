125d7dfa50fd599663751f294864c89ed6fb9986
jtquach1 <jtquach1@gmail.com>
2020-08-10 14:07:18 -0400

    Remove circle inside map marker

--- src/MapChart.tsx
+++ src/MapChart.tsx
@@ -141,14 +141,13 @@ const MapChart = (props: { functions: setters }): JSX.Element => {
                 }}
               >
                 <g
-                  fill="none"
+                  fill="#3786c2"
                   stroke="#3786c2"
                   strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   transform="translate(-12, -24)"
                 >
-                  <circle cx="12" cy="10" r="3" />
                   <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
                 </g>
                 {isVisible() && (
