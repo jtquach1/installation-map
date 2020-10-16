25bbf350d5bdb4eaf8dda9f55607d3df383ebb52
jtquach1 <jtquach1@gmail.com>
2020-10-16 18:11:06 -0400

    Create GitHub Action to automatically update waypoints.json - Updated Python script to     - not use APScheduler and to check Approved column but not include in final output     - add newest rows that are missing values in certain columns - Added new decryption script and encrypted credentials - Output src/waypoints.json instead of build/static/waypoints.json - Conditionally commit changes to waypoints.json if there are any, and if waypoints.json doesn't already exist

--- quickstart.py
+++ quickstart.py
@@ -5,24 +5,23 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import googlemaps
-from datetime import datetime
 import json
-from apscheduler.schedulers.blocking import BlockingScheduler
+from itertools import zip_longest
 
-### Google Sheets API ###
+# Google Sheets API
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 
 # The ID and range of a spreadsheet.
 SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
-COLS = "E:L"
+COLS = "A:A F:M"
 
-### Geocoding API ###
+# Geocoding API
 GEOCODING_API_KEY = os.getenv("GEOCODING_API_KEY")
 
 # Specified directory and file
-STATIC_DIR = "./build/static"
-WAYPOINTS = "%s/waypoints.json" % STATIC_DIR
+SRC_DIR = "./src"
+WAYPOINTS = "%s/waypoints.json" % SRC_DIR
 
 
 def create_waypoints(input_sheet):
@@ -50,8 +49,8 @@ def create_waypoints(input_sheet):
             )
     except FileNotFoundError:
         print('File "' + input_sheet + '" does not already exist. Creating.')
-        if not os.path.exists(STATIC_DIR):
-            os.makedirs(STATIC_DIR)
+        if not os.path.exists(SRC_DIR):
+            os.makedirs(SRC_DIR)
         final = []
     finally:
         keys = extract_keys()
@@ -208,7 +207,7 @@ def extract_values(scopes, spreadsheet_id, ranges):
     column_values = []
     for response in value_ranges:
         column_values.extend(response.get("values", []))
-    values_as_list_of_tuples = list(zip(*column_values))
+    values_as_list_of_tuples = list(zip_longest(*column_values, fillvalue=""))
     dump = json.dumps(values_as_list_of_tuples)
     row_values = json.loads(dump)
     return row_values
@@ -318,6 +317,7 @@ def remove_extraneous_columns(row):
     row.pop("City")
     row.pop("State / Province")
     row.pop("Country")
+    row.pop("Approved")
     return row
 
 
@@ -342,6 +342,4 @@ def main():
 
 
 if __name__ == "__main__":
-    scheduler = BlockingScheduler()
-    scheduler.add_job(main, trigger="cron", hour="*", minute="*", second="0")
-    scheduler.start()
+    main()
